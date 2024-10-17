06e69c8703157afbba7189f10efdcbf2b912dc5a
Shalginskaia Bozhena <103141829+Bozhenashalginskaia@users.noreply.github.com>
2024-10-17 15:45:27 +0300

    Update practice_5_1.py
    add comments

--- practice_5_1.py
+++ practice_5_1.py
@@ -1,7 +1,6 @@
 def capitalize_words(sentence):
     return sentence.title()
-
-# Пример использования
+#title - преобразование в заголовок
 sentence = "this is a sample sentence."
 capitalized_sentence = capitalize_words(sentence)
 print(capitalized_sentence)
